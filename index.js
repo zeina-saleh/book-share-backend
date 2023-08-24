2a7fa2b411153a8a0d156e882cf9a07618070c5b
zeinaCodes <eye_likemoon@hotmail.com>
2023-08-24 20:46:01 +0300

    <server> required routes

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express')
-const app = express()
 const mongooseConnect = require("./configs/mongoDB.connect");
 require("dotenv").config()
 
+const app = express()
 app.use(express.json())
 
-app.listen(8000, (err)=> {
+const authRouter = require("./routes/auth.routes")
+app.use("/auth", authRouter)
 
+const authMiddleware = require("./middleware/auth.middleware");
+
+const reviewRouter = require("./routes/review.routes")
+app.use("/", authMiddleware , reviewRouter)
+
+
+app.listen(8000, (err)=> {
+    if(err){
+        console.error(err)
+        return
+    }
     console.log('serving app on port 8000')
     mongooseConnect()
 })
