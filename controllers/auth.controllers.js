50aae831452bdc986a0b2de00664f79f5863bf05
zeinaCodes <eye_likemoon@hotmail.com>
2023-08-24 20:43:42 +0300

    <server> modifications to auth routes and controller

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken")
-const User = require("../models/users.model")
+const User = require("../models/users.model.js")
 
 const login = async (req, res)=>{
     const {email: login, password} = req.body;
@@ -8,34 +8,33 @@ const login = async (req, res)=>{
     if(!user) return res.status(404).send({message: "email/password incorrect"});
     const isValid = await bcrypt.compare(password, user.password);
     if(!isValid) return res.status(404).send({message: "email/password incorrect"});
-
-    const {password: hashedPassword, firstName, lastName, email, _id, ...userInfo} = user.toJSON();
-    const token = jwt.sign({firstName, lastName, email, _id}, process.env.JWT_SECRET)
-
+    const {password: hashedPassword, name, email, _id, ...userInfo} = user.toJSON();
+    const token = jwt.sign({name, email, _id}, process.env.JWT_SECRET)
     res.send({
         token,
         user: userInfo
     })
-
 }
-
-const register = async(req, res)=>{
-    const {password} = req.body
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({
-        ...req.body,
-        password: hashedPassword
-    });
-
-    user.save()
-
-    res.send(user)
-    
+const register = async (req, res) => {
+    console.log('req.body:', req.body);
+    const { password } = req.body;
+    if (!password) {
+      return res.status(400).send({ message: 'Password is missing' });
+    }
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({
+            ...req.body,
+            password: hashedPassword
+        });
+        await user.save();
+        res.send(user);
+    } catch (error) {
+        console.error('Error registering user:', error);
+        res.status(500).send({ message: 'Registration failed' });
+    }
 }
-
 const verify = (_, res)=>{
     res.send("Verfied")
 }
-
 module.exports = {login, register, verify}
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth.controllers");
-const authMiddleware = require("../middlewares/auth.middleware");
 
 router.post("/login", authController.login)
 router.post("/register", authController.register)
-router.get("/verify", authMiddleware, authController.verify)
+router.get("/verify", authController.verify)
 
 module.exports = router;
