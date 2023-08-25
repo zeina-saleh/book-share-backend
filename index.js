7f9d5a9e441ea2a097a0fffd239138181d356c8c
zeinaCodes <eye_likemoon@hotmail.com>
2023-08-25 07:47:20 +0300

    bug fixes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const mongooseConnect = require("./configs/mongoDB.connect");
 require("dotenv").config()
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 const authRouter = require("./routes/auth.routes")
